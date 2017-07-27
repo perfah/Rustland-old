3b68dd16a138dce3299cc34b212e6704ae005e25
Per Fahlander <perfah@kth.se>
2017-07-27 18:21:23 +0200

    Fixed utils::interpolation::InterpolationMethod to be object-safe (without associated consts) (https://github.com/rust-lang/rust/issues/29646)

--- src/rustland/utils/interpolation/methods.rs
+++ src/rustland/utils/interpolation/methods.rs
@@ -1,38 +1,32 @@
 use common::definitions::DefaultNumericType;
 
 pub trait InterpolationMethod: Send{
-    const left_bound: DefaultNumericType;
-    const right_bound: DefaultNumericType;
-
     fn calc_progression(&self, x: DefaultNumericType) -> DefaultNumericType;
-    fn get_left_bound(&self) -> DefaultNumericType { Self::left_bound }
-    fn get_right_bound(&self) -> DefaultNumericType { Self::right_bound }
+    fn get_left_bound(&self) -> DefaultNumericType;
+    fn get_right_bound(&self) -> DefaultNumericType;
 }
 
 /// An interpolation method that keeps the motion speed at a constant rate.
 pub struct LinearInterpolator;
 impl InterpolationMethod for LinearInterpolator{
-    const left_bound: DefaultNumericType = 0f32;
-    const right_bound: DefaultNumericType = 10f32;
-
     fn calc_progression(&self, x: f32) -> f32 { x }
+    fn get_left_bound(&self) -> DefaultNumericType { 0f32 }
+    fn get_right_bound(&self) -> DefaultNumericType { 10f32 }
 }
 
 
 /// An interpolation method that accelerates the motion quadratically.
 pub struct QuadraticInterpolator;
 impl InterpolationMethod for QuadraticInterpolator{
-    const left_bound: DefaultNumericType = 0f32;
-    const right_bound: DefaultNumericType = 10f32;
-
     fn calc_progression(&self, x: f32) -> f32 { x * x }
+    fn get_left_bound(&self) -> DefaultNumericType { 0f32 }
+    fn get_right_bound(&self) -> DefaultNumericType { 10f32 }
 }
 
 /// An interpolation method that decelerates the motion.
 pub struct DecelerationInterpolator;
 impl InterpolationMethod for DecelerationInterpolator{
-    const left_bound: DefaultNumericType = 0f32;
-    const right_bound: DefaultNumericType = 1.57f32;
-
     fn calc_progression(&self, x: f32) -> f32 { x.sin() * x.sin() }
+    fn get_left_bound(&self) -> DefaultNumericType { 0f32 }
+    fn get_right_bound(&self) -> DefaultNumericType { 1.57f32 }
 }
